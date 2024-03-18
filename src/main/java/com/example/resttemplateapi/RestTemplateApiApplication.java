cc03688806d537ba7e225716581cc45e31e39c9e
marmiecode <mieczkowska.maartyna@gmail.com>
2024-03-18 20:21:42 +0100

    Added URL configuration

--- src/main/java/com/example/resttemplateapi/RestTemplateApiApplication.java
+++ src/main/java/com/example/resttemplateapi/RestTemplateApiApplication.java
@@ -1,13 +1,21 @@
 package com.example.resttemplateapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class RestTemplateApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(RestTemplateApiApplication.class, args);
-	}
-
+    @Autowired
+    EdSheeranProxy edSheeranClient;
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateApiApplication.class, args);
+    }
+    @EventListener(ApplicationStartedEvent.class)
+    public void makeRequestEdSheeranEndpoint(){
+        String response = edSheeranClient.makeEdSheeranRequest("edsheeran", 1);
+        System.out.println(response);
+    }
 }
